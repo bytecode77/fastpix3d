2a28137f2d5600bbf0910a851e46ea0caff07623
Martin Fischer <mail@martinfischer.net>
2026-06-07 21:17:39 +0200

    Optimize Graphics class

--- FastPix3D/2D/Graphics.cpp
+++ FastPix3D/2D/Graphics.cpp
@@ -17,24 +17,49 @@ void Graphics::FillRectangle(int32 x, int32 y, int32 width, int32 height, const 
 }
 void Graphics::FillRectangle(int32 x, int32 y, int32 width, int32 height, const Color &color, float alpha) const
 {
+	if (alpha <= 0)
+	{
+		return;
+	}
+
 	int32 xFrom = Math::Clamp(x, 0, _Width - 1);
 	int32 yFrom = Math::Clamp(y, 0, _Width - 1);
 	int32 xTo = Math::Clamp(x + width, 0, _Width - 1);
 	int32 yTo = Math::Clamp(y + height, 0, _Width - 1);
 
-	int32 alphaA = Math::Clamp((int32)(alpha * 256), 0, 256);
-	int32 alphaB = 256 - alphaA;
+	Color *frameBuffer = &_Pixels[xFrom + yFrom * _Width];
+	int32 stride = _Width - xTo + xFrom;
 
-	for (int32 py = yFrom; py < yTo; py++)
+	if (alpha >= 1)
 	{
-		Color *frameBuffer = &_Pixels[xFrom + py * _Width];
-
-		for (int32 px = xFrom; px < xTo; px++)
+		for (int32 py = yFrom; py < yTo; py++)
 		{
-			frameBuffer->B = (frameBuffer->B * alphaB + color.B * alphaA) >> 8;
-			frameBuffer->G = (frameBuffer->G * alphaB + color.G * alphaA) >> 8;
-			frameBuffer->R = (frameBuffer->R * alphaB + color.R * alphaA) >> 8;
-			frameBuffer++;
+			for (int32 px = xFrom; px < xTo; px++)
+			{
+				*frameBuffer++ = color;
+			}
+
+			frameBuffer += stride;
+		}
+	}
+	else
+	{
+		int32 alphaA = Math::Clamp((int32)(alpha * 256), 0, 256);
+		int32 alphaB = 256 - alphaA;
+
+		for (int32 py = yFrom; py < yTo; py++)
+		{
+			Color *frameBuffer = &_Pixels[xFrom + py * _Width];
+
+			for (int32 px = xFrom; px < xTo; px++)
+			{
+				frameBuffer->B = (frameBuffer->B * alphaB + color.B * alphaA) >> 8;
+				frameBuffer->G = (frameBuffer->G * alphaB + color.G * alphaA) >> 8;
+				frameBuffer->R = (frameBuffer->R * alphaB + color.R * alphaA) >> 8;
+				frameBuffer++;
+			}
+
+			frameBuffer += stride;
 		}
 	}
 }
@@ -50,17 +75,20 @@ void Graphics::DrawString(int32 x, int32 y, const char *text, const Color &color
 
 	for (int32 i = 0; i < textLength; i++)
 	{
-		char character = text[i];
-		int32 characterX = (text[i] & 15) * _Font->Width;
-		int32 characterY = (text[i] >> 4) * _Font->Height;
-		vint2 characterDimension = _Font->CharacterDimensions[character];
+		char c = text[i];
+		int32 characterX = (c & 15) * _Font->Width;
+		int32 characterY = (c >> 4) * _Font->Height;
+		vint2 characterDimension = _Font->CharacterDimensions[c];
 		int32 characterWidth = characterDimension.Y - characterDimension.X;
+
+		Color *frameBuffer = &_Pixels[x + y * _Width];
+		int32 stride = Width - characterWidth;
+
+		byte *bitmapPtr = &_Font->Buffer[characterDimension.X + characterX + characterY * (_Font->Width << 4)];
+		int32 bitmapStride = (_Font->Width << 4) - characterWidth;
 
 		for (int32 py = 0; py < _Font->Height; py++)
 		{
-			byte *bitmapPtr = &_Font->Buffer[characterDimension.X + characterX + (py + characterY) * (_Font->Width << 4)];
-			Color *frameBuffer = &_Pixels[x + (y + py) * _Width];
-
 			for (int32 px = 0; px < characterWidth; px++)
 			{
 				int32 alphaA = *bitmapPtr++;
@@ -71,6 +99,9 @@ void Graphics::DrawString(int32 x, int32 y, const char *text, const Color &color
 				frameBuffer->R = (frameBuffer->R * alphaB + color.R * alphaA) >> 8;
 				frameBuffer++;
 			}
+
+			frameBuffer += stride;
+			bitmapPtr += bitmapStride;
 		}
 
 		x += characterWidth + _Font->CharacterSpacing;
@@ -80,18 +111,13 @@ int32 Graphics::MeasureString(const char *text) const
 {
 	if (!_Font) throw;
 
-	int32 textLength = lstrlenA(text);
-	int32 width = 0;
+	int32 length = lstrlenA(text);
+	int32 width = _Font->CharacterSpacing * Math::Max(length - 1, 0);
 
-	for (int32 i = 0; i < textLength; i++)
+	for (int32 i = 0; i < length; i++)
 	{
-		char character = text[i];
-		width += _Font->CharacterDimensions[character].Y - _Font->CharacterDimensions[character].X;
-
-		if (i < textLength - 1)
-		{
-			width += _Font->CharacterSpacing;
-		}
+		char c = text[i];
+		width += _Font->CharacterDimensions[c].Y - _Font->CharacterDimensions[c].X;
 	}
 
 	return width;
